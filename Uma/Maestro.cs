31925306b28211f0c6a343d510933e038e7dd9cc
oswalderon <oswalderon@gmail.com>
2020-10-08 08:15:41 -0600

    Creación de clases (Maestro, Nota, Grupo)

--- Uma/Maestro.cs
+++ Uma/Maestro.cs
@@ -8,5 +8,62 @@ namespace Uma
 {
     class Maestro
     {
+        static void Mostrar_Grupos()
+        {
+
+        }
+
+        static void Edit_Conection()
+        {
+
+        }
+
+        static void Crear()
+        {
+
+        }
+
+        static void Destruir()
+        {
+
+        }
+
+        static void Perfil()
+        {
+
+        }
+    }
+
+    class Grupos
+    {
+        private string id_grupo;
+        private string nombre_grupo;
+        private string id_maestro;
+        private int max_grup;
+
+        public string ID_Grupo
+        {
+            get { return id_grupo; }
+            set { id_grupo = value; }
+        }
+
+        public string Nombre_Grupo
+        {
+            get { return nombre_grupo; }
+            set { nombre_grupo = value; }
+        }
+
+        public string ID_Maestro
+        {
+            get { return id_maestro; }
+            set { id_maestro = value; }
+        }
+
+        public int Max_Grup
+        {
+            get { return max_grup; }
+            set { max_grup = value; }
+        }
+
     }
 }
